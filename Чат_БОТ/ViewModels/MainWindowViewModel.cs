9e65056e9e158a8f9a3722407e84f1e10832b83f
Михаил Александрович Александренко <98051902+Bobchinskey@users.noreply.github.com>
2022-03-15 23:20:39 +0400

    Добавьте файлы проекта.

---
+++ Чат_БОТ/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Чат_БОТ.ViewModels.Base;
+
+namespace Чат_БОТ.ViewModels
+{
+    internal class MainWindowViewModel : ViewModelBase
+    {
+        #region Имя Бота : NameBot
+
+        private string _NameBot = "Чат бот";
+
+        /// <summary>NameBot</summary>
+        public string NameBot
+        {
+            get => _NameBot;
+            set => Set(ref _NameBot, value);
+        }
+
+        #endregion
+    }
+}
